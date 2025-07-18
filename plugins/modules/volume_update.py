d94117b9ed141fc71e0fd43181312f2f2b03845a
KarteeshKumarV <vkarteesh@gmail.com>
2025-07-18 15:19:12 +0530

    Add Volume Info, VolumeType Info and Volume Update features
    Signed-off-by: KarteeshKumarV <vkarteesh@gmail.com>

--- plugins/modules/volume_update.py
+++ plugins/modules/volume_update.py
@@ -7,7 +7,11 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 
 DOCUMENTATION = '''
 ---
+<<<<<<< HEAD
 module: volume_update
+=======
+module: volume_attach
+>>>>>>> 49ef494 (Add Volume Info, VolumeType Info and Volume Update features)
 author:
     - Karteesh Kumar Vipparapelli (@vkarteesh)
 short_description: Performs the Volume Update Operations
@@ -30,7 +34,11 @@ options:
   enable_sharing_vm:
     description:
       - Enable sharing between the VMs of the Volume
+<<<<<<< HEAD
     type: bool
+=======
+    type: str    
+>>>>>>> 49ef494 (Add Volume Info, VolumeType Info and Volume Update features)
 
 '''
 
@@ -51,7 +59,10 @@ EXAMPLES = '''
          ibm.powervc.volume_update:
             auth: "{{ auth }}"
             name: "VOLUME_NAME"
+<<<<<<< HEAD
             size: "SIZE"
+=======
+>>>>>>> 49ef494 (Add Volume Info, VolumeType Info and Volume Update features)
             validate_certs: no
          register: result
        - debug:
@@ -65,7 +76,10 @@ EXAMPLES = '''
          ibm.powervc.volume_update:
             cloud: "CLOUD_NAME"
             id: "VOLUME_ID"
+<<<<<<< HEAD
             enable_sharing_vm: "ENABLE_SHARING_VM"
+=======
+>>>>>>> 49ef494 (Add Volume Info, VolumeType Info and Volume Update features)
             validate_certs: no
          register: result
        - debug:
@@ -79,7 +93,10 @@ EXAMPLES = '''
          ibm.powervc.volume_update:
             cloud: "CLOUD_NAME"
             name: "VOLUME_NAME"
+<<<<<<< HEAD
             size: "SIZE"
+=======
+>>>>>>> 49ef494 (Add Volume Info, VolumeType Info and Volume Update features)
             validate_certs: no
          register: result
        - debug:
@@ -96,7 +113,11 @@ class VolumeUpdateModule(OpenStackModule):
         name=dict(),
         id=dict(),
         size=dict(type='int'),
+<<<<<<< HEAD
         enable_sharing_vm=dict(type='bool'),
+=======
+        enable_sharing_vm=dict(),
+>>>>>>> 49ef494 (Add Volume Info, VolumeType Info and Volume Update features)
     )
     module_kwargs = dict(
         supports_check_mode=True,
